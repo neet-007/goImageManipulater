bf23c771e3bfe2596bfb9159f5b0538a715e3f79
neet-007 <moayedeffect@gmail.com>
2024-09-05 17:47:58 +0300

    huffman table intial

--- jpegDecoder.go
+++ jpegDecoder.go
@@ -108,9 +108,71 @@ func decodeHuffman(data *os.File) error {
 		elements = append(elements, elem...)
 	}
 
+	newHuffman := NewHuffman()
+
+	newHuffman.getHuffmanBits(len_buf, elements)
+
 	fmt.Printf("haeder %d\n", int(buf[0])<<8|int(buf[1]))
 	fmt.Printf("length %d\n", len_buf)
 	fmt.Printf("elemnts %d\n", len(elements))
+	fmt.Printf("roots %v\n", newHuffman.root)
+	fmt.Printf("elents %v\n", newHuffman.elemnts)
 
 	return nil
+}
+
+type Huffman struct {
+	root    []interface{}
+	elemnts []byte
+}
+
+func NewHuffman() *Huffman {
+	return &Huffman{
+		root: make([]interface{}, 0),
+	}
+}
+
+func (hf *Huffman) bitsFromLength(root *[]interface{}, element byte, pos int) bool {
+	if len(*root) == 0 {
+		*root = append(*root, make([]interface{}, 0))
+	}
+
+	if pos == 0 {
+		if len(*root) < 2 {
+			*root = append(*root, element)
+			return true
+		}
+		return false
+	}
+
+	for i := 0; i < 2; i++ {
+		if len(*root) == i {
+			*root = append(*root, make([]interface{}, 0))
+		}
+		var childRoot []interface{}
+		if existing, ok := (*root)[i].([]interface{}); ok {
+			childRoot = existing
+		} else {
+			childRoot = make([]interface{}, 0)
+		}
+
+		if hf.bitsFromLength(&childRoot, element, pos-1) {
+			(*root)[i] = childRoot
+			return true
+		}
+	}
+
+	return false
+}
+
+func (hf *Huffman) getHuffmanBits(lengths []byte, elements []byte) {
+	hf.elemnts = elements
+
+	ii := 0
+	for i := 0; i < len(lengths); i++ {
+		for _ = range lengths[i] {
+			hf.bitsFromLength(&hf.root, elements[ii], i)
+			ii++
+		}
+	}
 }
